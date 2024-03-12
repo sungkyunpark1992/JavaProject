fb4e452bef3e70ceaa15de992def8c0116a496f8
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2024-03-12 10:03:05 +0900

    240311-별찍기 문제

--- FirstJava2/src/quiz/ForStarDiamond.java
+++ FirstJava2/src/quiz/ForStarDiamond.java
@@ -1,12 +1,14 @@
 package quiz;
 
+import java.util.Scanner;
+
 public class ForStarDiamond {
 
 	public static void main(String[] args) {
 		// 다이아몬드 찍기
 		// 1. 다음과 같은 형태로 별직기를 진행해 보세요.  
 		/*          힌트  : 공백, 별, 단(줄)
-		      *
+		      *		분기점을 기점으로 반전되는걸 플래그기법?이라고 한다.
 		     ***
 		    *****
 		   *******
@@ -16,10 +18,62 @@ public class ForStarDiamond {
 		     ***
 		      *
 		 */
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("줄 수를 입력해주세요 : ");
+		int dan = scan.nextInt();
+		
+//		for(int i =0; i < dan; i++) {
+//			for(int j = 0; j < dan - i - 1; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0; j<i*2+1;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 0; i <dan-1; i++) {
+//			for(int j=0; j<i+1;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0; j<2*(dan-i-1)-1;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();			
+//		}
+//		
+		
+		
 
 		// 2. 1번의 코드를 활용하여 줄 수를 입력 받아서 그 줄 수 에 맞는 다이아몬드 별을 찍는 
  		//   프로그램을 작성해 보세요. (단, 줄은 홀수로만 입력 받아야 합니다.)
-
+		//변수선언 : sp(여백), st(별), 반전을 위한 flag(boolean) - true(삼각형), false(역삼각형)
+		int sp = dan/2;
+		int st = 1;
+		boolean flag= true;
+		
+		for(int i = 0; i < dan; i++) {
+			//여백처리
+			for(int j=0; j<sp; j++) {
+				System.out.print(" ");
+			}
+			//별찍기
+			for(int j=0; j<st; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			if(i==(dan/2))flag = false;
+			if(flag) {
+				sp-=1; st+=2;
+			}else {
+				sp+=1; st-=2;
+			}
+		}
+		
+		
+		scan.close();
+		
 	}
 
 }
